c7266159791416301b6a2a50aa4e75e7d67b7983
shreep02 <developshree02@gmail.com>
2026-01-02 23:03:49 +0100

    Mobile responsivnes and font corrections

--- sarthak-web/src/app/navigation/navigation.component.ts
+++ sarthak-web/src/app/navigation/navigation.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
+  standalone: true,
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
